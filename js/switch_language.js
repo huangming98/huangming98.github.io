938ef7448f5fd8ddf34bc15f2cc0658d35bb4ed3
huangming98 <huangming98@163.com>
2022-09-13 22:48:08 +0800

    Site updated: 2022-09-13 22:48:08

--- js/switch_language.js
+++ js/switch_language.js
@@ -1,12 +1,13 @@
 var _prevOnload = window.onload;
 
 window.onload = function () {
-    var switchLang = document.getElementsByClassName("menu-item menu-item-switch-to-english")[0];
+    var switchLang = document.getElementsByClassName("menu-item menu-item-switch-to-chinese")[0];
     switchLang.onclick = function () {
         var href = window.location.href;
-        var includesKeywords = href.includes("/homepage/") || href.includes("/home/")|| href.includes("/publications/") || href.includes("/resources/")|| href.includes("/link/")|| href.includes("/archives/");
+        var includesKeywords = href.includes("/homepage/") || href.includes("/home/")|| href.includes("/publications/") || href.includes("/resources/")|| href.includes("/link/")|| href.includes("/archives/")|| href.includes("/Switch to Chinese/");
         if (includesKeywords) {
-            window.location.href = href.replace('.io/', '.io/cn/');
+            window.location.href = href.replace('github.io/', 'github.io/cn/');
+
         }
         else {
             window.location.href = "https://huangming98.github.io/cn/";
